8076bfb57d03f620fb435e04033497126a9d3e6b
fkolender <hedrite@hotmail.com>
2023-03-14 00:18:41 -0300

    pls

--- portfolio/components/Footer.jsx
+++ portfolio/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     
     <footer>
       <div className="flex justify-center space-x-2 pb-2">
-      <p>Check my GitHub</p>
+      <h2>Check out my GitHub</h2>
       <a href='https://github.com/fkolender/'><img src="github.svg" height={32} width={32}></img></a>
       <a href='https://www.linkedin.com/in/francisco-kolender-200a41234/'><img src="linkedin.svg" height={32} width={32}></img></a>
       </div>
